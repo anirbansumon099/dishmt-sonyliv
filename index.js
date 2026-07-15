d9ed395577af73f308ca1c284a71d6e6ed9df1f5
অনির্বান সুমন <anirbansumon099@gmail.com>
2026-07-15 20:12:37 +0600

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ export default {
       "User-Agent": "Mozilla/5.0 (Android 13; Mobile; rv:150.0) Gecko/150.0 Firefox/150.0",
       "Accept": "*/*",
       "Accept-Language": "en-US",
-      "Referer": "https://allinonereborn.online/sony/ptest.php?id=sony-ten-4",
+      "Referer": "https://allinonereborn2.online/sony/ptest.php?id=sony-ten-4",
       "Sec-Fetch-Dest": "empty",
       "Sec-Fetch-Mode": "cors",
       "Sec-Fetch-Site": "same-origin"
